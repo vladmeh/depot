92869efd47788ea8138e78c7af89c10e91025ddc
vladmeh home <vladmeh@gmail.com>
2014-10-11 01:39:54 +0400

    Task-D-CartCreation

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,16 @@
 class StoreController < ApplicationController
+  # def increment_counter
+  #   if session[:counter].nil?
+  #     session[:counter] = 0
+  #   end
+  #   session[:counter] += 1
+  #   if session[:counter] >= 5
+  #     return session[:counter]
+  #   end
+  # end
+
   def index
+    # @count = increment_counter
     @products = Product.order(:title)
   end
 end
