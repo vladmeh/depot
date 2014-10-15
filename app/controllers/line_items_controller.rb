fa8f1767e78e9a5a013a2ba3fde59be9c5e7857d
vladmeh home <vladmeh@gmail.com>
2014-10-16 00:06:18 +0400

    Add a Dash of Ajax

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,8 +30,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        # session[:counter] = nil
         format.html { redirect_to store_url }
+        format.js {@current_item = @line_item}
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
